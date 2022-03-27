28f6c046b5ca3f085ccaed2bb59a088d5e0e3498
Pablo Pardo <pablopardoalcocer@gmail.com>
2022-03-27 00:25:04 -0400

    Init configuration

---
+++ src/todo/Todo.tsx
@@ -0,0 +1,12 @@
+export const Todo = () => {
+  return (
+    <>
+      <h1>Todo:</h1>
+      <ul>
+        <li>TODO 1</li>
+        <li>TODO 2</li>
+        <li>TODO 3</li>
+      </ul>
+    </>
+  );
+};
